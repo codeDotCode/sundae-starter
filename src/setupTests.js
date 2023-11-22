ef2adf15570e2daf6d41b3d03faf94f4ab37826b
LeshchuDev <leshchudev@gmail.com>
2023-11-22 18:57:17 +0100

    Established API mocking library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom";
+
+import { beforeAll, afterEach, afterAll } from 'vitest'
+import { server } from './mocks/server'
+
+
+//Establish API mocking before all tests
+beforeAll(() => server.listen())
+
+// reset any request handlers that we may add during the tests,
+//so they don't affect other tests
+afterEach(() => server.resetHandlers())
+
+//clean up after the tests are finished
+afterAll(() => server.close())
